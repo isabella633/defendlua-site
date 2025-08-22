0a1b083b3bd5d99d9ee7d72b5dd37b17314f9dce
isabella633 <120885253+isabella633@users.noreply.github.com>
2025-08-22 14:41:52 -0400

    Delete script.lua

--- script.lua
+++
@@ -1,8 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-const encoded = params.get('c');
-if(encoded){
-    const code = decodeURIComponent(escape(atob(encoded)));
-    document.open();
-    document.write(`<pre>${code}</pre>`);
-    document.close();
-}
