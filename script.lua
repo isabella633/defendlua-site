e36417c7e7c4f9b95dccaa8894ecc343e1b98e9b
isabella633 <120885253+isabella633@users.noreply.github.com>
2025-08-22 14:22:39 -0400

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,10 @@
+<script>
+const params = new URLSearchParams(window.location.search);
+const encoded = params.get('c');
+if(encoded){
+    const code = decodeURIComponent(escape(atob(encoded)));
+    document.open();
+    document.write(`<pre>${code}</pre>`);
+    document.close();
+}
+</script>
